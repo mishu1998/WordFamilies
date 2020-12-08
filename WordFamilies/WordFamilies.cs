ec572b8ab85e2b6d140c6e6f74f38cf56321e855
Mihai Ionita <MAI158@student.anglia.ac.uk>
2020-12-08 21:26:01 +0000

    Restructure

---
+++ WordFamilies/WordFamilies.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace WordFamilies
+{
+    public class WordFamilies
+    {
+        public string pattern;
+        public List<Words> wordList = new List<Words>();
+        public double score = 0;
+
+        public void DetermineScore()
+        {
+            double lettersScore = 0;
+
+            for (int i = 0; i < wordList.Count; i++)
+            {
+                double uniqueLetters = 0;
+
+                for (int j = 0; j < 26; j++)
+                {
+                    if (wordList[i].letterFreq[j] != 0 && Program.lettersGuessed.Contains((char)i) == false)
+                        uniqueLetters++;
+                }
+                double consScore = (double)(uniqueLetters - wordList[i].vowels) / 26;
+                double vowelScore = (double)wordList[i].vowels / 26;
+                lettersScore += consScore * 0.7 + vowelScore * 0.3;
+            }
+            double sizeScore = (double)wordList.Count / Program.wordList.Count;
+            lettersScore /= wordList.Count;
+            score = sizeScore * 0.6 + lettersScore * 0.4;
+        }
+    }
+}
